214be19d9da02357d1748073e29a6c957fc382bd
Ilya Lisov <maks.kramanenko@mail.ru>
2023-01-09 00:33:14 +0300

    feat: Implement Library text processor

--- src/main/java/com/example/reader/processor/LibraryProcessor.java
+++ src/main/java/com/example/reader/processor/LibraryProcessor.java
@@ -1,12 +1,35 @@
 package com.example.reader.processor;
 
+import com.ezylang.evalex.EvaluationException;
+import com.ezylang.evalex.Expression;
+import com.ezylang.evalex.parser.ParseException;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LibraryProcessor implements Processor {
     @Override
     public String process(String text) {
-        //TODO implement library processor
+        int startOfOperation = 0;
+        int endOfOperation = text.length() - 1;
+
+        for(int i = 0; i < text.length(); i++) {
+            if(text.charAt(i) >= 42 && text.charAt(i) < 58) {
+                startOfOperation = i;
+                for(int j = i + 1; j < text.length(); j++) {
+                    if((text.charAt(i) < 42 || text.charAt(i) >= 58) && text.charAt(i) != ' ') {
+                        endOfOperation = j;
+                        break;
+                    }
+                }
+                Expression expression = new Expression(text.substring(startOfOperation, endOfOperation + 1));
+                try {
+                    text = text.substring(0, i) + expression.evaluate().getNumberValue() + text.substring(endOfOperation + 1);
+                } catch (EvaluationException | ParseException e) {
+                }
+                i = endOfOperation;
+                endOfOperation = text.length() - 1;
+            }
+        }
         return text;
     }
 }
